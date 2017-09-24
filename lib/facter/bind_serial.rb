4e72869f7293e542523d632664df979406c7f060
Jason Cox <jasonc@buildingiq.com>
2017-09-24 11:42:17 +1000

    adding in fact for returning the serials for zone files so that they can be formated as per standards

---
+++ lib/facter/bind_serial.rb
@@ -0,0 +1,34 @@
+require 'facter'
+kernel = Facter.value(:kernel)
+case kernel
+when /Linux/
+	os = Facter.value(:osfamily)
+	Facter.add(:bind_serials) do
+		bind_serials = {}
+		case os
+		when /RedHat/
+			all_zones = %x[ grep file /etc/named/named.conf.local  |cut -d\\\" -f2 ]
+		when /Debian/
+			all_zones = %x[ grep file /etc/bind/named.conf.local  |cut -d\\\" -f2 ]
+		end
+		all_zones.split("\n").each do |zone|
+			serial_line = File.open(zone).grep(/Serial/)
+			serial_split = serial_line[0].delete("\t").split(";")
+			serial =serial_split[0]
+			dnsdate = serial[0,8]
+			dnsserial = serial[8,10]
+
+
+			_bind_serials = {}
+			_bind_serials['zone'] = zone
+			_bind_serials['serial'] = serial
+			_bind_serials['dnsdate'] = dnsdate 
+			_bind_serials['dnsserial'] = dnsserial
+			bind_serials[zone] = _bind_serials
+		end
+		setcode do
+			bind_serials
+		end
+	end
+end
+
