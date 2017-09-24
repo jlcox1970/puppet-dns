53543dc9bc3f7d02f348698d21032f243841318b
Jason Cox <jasonc@buildingiq.com>
2017-09-24 13:13:44 +1000

    typo

--- manifests/zone.pp
+++ manifests/zone.pp
@@ -284,8 +284,8 @@ define dns::zone (
     $zone_year = inline_template('<%= Time.now.year %>')
     $zone_month = inline_template('<%= sprintf "%02d" , Time.now.month %>')
     $zone_day = inline_template('<%= sprintf "%02d" , Time.now.day %>')
-
-  $current_serial = $::bind_serial["$zone"]
+$zone_name = $name
+  $current_serial = $::bind_serial["$zone_name"]
     
   notify {"full bind_serail ::: $::bind_serial":}
   notify {"$zone current serial is $current_serial ":}
