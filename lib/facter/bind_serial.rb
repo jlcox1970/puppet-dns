012b0b31bdd5cc4cd42bb03a6f3c318eb3cfaf6b
Jason Cox <jasonc@buildingiq.com>
2017-09-24 11:51:29 +1000

    lets make the zone more intuative

--- lib/facter/bind_serial.rb
+++ lib/facter/bind_serial.rb
@@ -2,34 +2,34 @@ require 'facter'
 kernel = Facter.value(:kernel)
 case kernel
 when /Linux/
-	os = Facter.value(:osfamily)
-	Facter.add(:bind_serials) do
-		bind_serials = {}
-		case os
-		when /RedHat/
-			all_zones = %x[ grep file /etc/named/named.conf.local  |cut -d\\\" -f2 ]
-		when /Debian/
-			all_zones = %x[ grep file /etc/bind/named.conf.local  |cut -d\\\" -f2 ]
-		end
-		all_zones.split("\n").each do |zone|
-			serial_line = File.open(zone).grep(/Serial/)
-			serial_split = serial_line[0].delete("\t").split(";")
-			serial =serial_split[0]
-			dnsdate = serial[0,8]
-			dnsserial = serial[8,10]
-			zone = zone.split("db.")
-			_zone = zone[1]
+        os = Facter.value(:osfamily)
+        Facter.add(:bind_serials) do
+                bind_serials = {}
+                case os
+                when /RedHat/
+                        all_zones = %x[ grep file /etc/named/named.conf.local  |cut -d\\\" -f2 ]
+                when /Debian/
+                        all_zones = %x[ grep file /etc/bind/named.conf.local  |cut -d\\\" -f2 ]
+                end
+                all_zones.split("\n").each do |zone|
+                        serial_line = File.open(zone).grep(/Serial/)
+                        serial_split = serial_line[0].delete("\t").split(";")
+                        serial =serial_split[0]
+                        dnsdate = serial[0,8]
+                        dnsserial = serial[8,10]
+                        zone_split = zone.split("db.")
+                        zone_name = zone_split[1]
 
-			_bind_serials = {}
-			_bind_serials['zone'] = _zone
-			_bind_serials['serial'] = serial
-			_bind_serials['dnsdate'] = dnsdate 
-			_bind_serials['dnsserial'] = dnsserial
-			bind_serials[zone] = _bind_serials
-		end
-		setcode do
-			bind_serials
-		end
-	end
+                        _bind_serials = {}
+                        _bind_serials['zone_file'] = zone
+                        _bind_serials['serial'] = serial
+                        _bind_serials['dnsdate'] = dnsdate
+                        _bind_serials['dnsserial'] = dnsserial
+                        bind_serials[zone_name] = _bind_serials
+                end
+                setcode do
+                        bind_serials
+                end
+        end
 end
 
