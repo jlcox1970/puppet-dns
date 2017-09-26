fa23d819793b623ce4d74b1ad1a1bba9c024876e
Jason Cox <jasonc@buildingiq.com>
2017-09-26 12:36:20 +1000

    add in the dnssec key gen

---
+++ manifests/dnssec/keys.pp
@@ -0,0 +1,10 @@
+define dns::dnssec::keys ($zone,$bind_dir,$urandom = false)
+{
+  if ( $urandom == true ){
+    $random = " -r /dev/urandom"
+  }
+  exec {"dnssec-keygen $random -a RSASHA256 -b 2048 -3 $zone":
+    cwd      => $bind_dir,
+    onceonly =>  true
+  }
+}
